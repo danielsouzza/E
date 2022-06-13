a973784d3c4847201d8295297d1982bed7870a35
danielsouzza <daniel.lima.souzza@gmail.com>
2022-06-13 18:07:22 -0300

    BubbleSort adicionado

--- Main.c
+++ Main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include "mergeSort.h"
+#include "bubbleSort.h"
 
 const int t = 80000; // Tamanho do vetor
 
@@ -46,13 +47,25 @@ void invertVector(int vetor[])
     }
 }
 
-double caso(int vetor[], int end)
+double casosMerge(int vetor[], int end)
 {
     time_t t_start; // t_start pega o tempo inicial
     time_t t_end;   // t_end pega o tempo final
 
     t_start = time(NULL);
     mergeSort(vetor, 0, end); // Chama o algoritmo que está em [mergeSort.h]
+    t_end = time(NULL);
+
+    return difftime(t_end,t_start);
+}
+
+double casoBubble(int vetor[])
+{
+    time_t t_start; // t_start pega o tempo inicial
+    time_t t_end;   // t_end pega o tempo final
+
+    t_start = time(NULL);
+    bubbleSort(vetor, t); // Chama o algoritmo que está em [mergeSort.h]
     t_end = time(NULL);
 
     return difftime(t_end,t_start);
@@ -67,13 +80,25 @@ void main()
     generateVectores(vetor1,vetor2,vetor3, vetor4, vetor5, vetor6, vetor7);
 
     // Merge Sort
-    tempo[0] = caso(vetor1, end);
-    tempo[1] = caso(vetor1, end);
+    tempo[0] = casosMerge(vetor1, end);
+    tempo[1] = casosMerge(vetor1, end);
     invertVector(vetor1);
-    tempo[2] = caso(vetor1, end);
+    tempo[2] = casosMerge(vetor1, end);
 
 
-    printf("O tempo de ordenaçao no caso medio.: %fs\n", tempo[0]);  
-    printf("O tempo de ordenaçao no melhor caso: %fs\n", tempo[1]);
-    printf("O tempo de ordenaçao no pior caso..: %fs\n", tempo[2]);
+    printf("MergeSort no caso médio.: %fs\n", tempo[0]);  
+    printf("MergeSort no melhor caso: %fs\n", tempo[1]);
+    printf("MergeSort no pior caso..: %fs\n", tempo[2]);
+
+    // Bubblesort
+    tempo[0] = casoBubble(vetor2);
+    tempo[1] = casoBubble(vetor2);
+    invertVector(vetor1);
+    tempo[2] = casoBubble(vetor2);
+
+    printf("BubbleSort caso médio.: %fs\n", tempo[0]);  
+    printf("BubbleSort melhor caso: %fs\n", tempo[1]);
+    printf("BubbleSort pior caso..: %fs\n", tempo[2]);
 }
+
+
